11c9330f4b645ee7bc189dae5e5dc7cae712fbe8
Jeremiah Utecht <j.utecht@improbablesoftware.co>
2015-04-10 09:42:44 -0500

    Revert 1b1e973..1257d46
    This rolls back to commit 1b1e97395af3ff7012e1792f25f73cd624678161.

--- app/models/subscription_plan.rb
+++
@@ -1,7 +0,0 @@
-class SubscriptionPlan < ActiveRecord::Base
-  include Payola::Plan
-
-  def redirect_path(subscription)
-    '/'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  mount Payola::Engine => '/payola', as: :payola
   devise_for :users
-  resources :subscriptions
   resources :articles do
     resources :comments
   end
